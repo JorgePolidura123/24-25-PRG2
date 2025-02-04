cedb9c7ac10c9bc5aaa638ba28878f0419135618
JorgePolidura123 <144842516+JorgePolidura123@users.noreply.github.com>
2025-02-04 14:05:05 +0100

    Codigo empezado

--- entregas/poliduraJorge/reto001/reto001.java
+++ entregas/poliduraJorge/reto001/reto001.java
@@ -1,1 +1,30 @@
+import java.until.Scanner;
 
+
+public class escalasAcordes{
+
+        private static final String[] notas = {
+                "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"
+            };
+       
+        public static void main(String[] args){
+
+            
+
+            int nota = scanner.nextInt();
+
+            int[] patronEscala = {2,2,1,2,2,2,1};
+
+            public static int obtenerIndice  (){
+                    for(i=0; i<notas.lenght;i++){
+                        if (notas[i].equals(nota)) {
+                            return i;
+                    }
+
+            }
+                return -1; // Retorna -1 si no se encuentra la nota
+        }
+       
+ 
+
+}
